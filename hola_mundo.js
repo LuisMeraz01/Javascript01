420b5731516966c53b380fc5375317438a14cc41
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-09-29 08:33:48 -0700

    Imprimir 1000 veces nodejs

--- hola_mundo.js
+++ hola_mundo.js
@@ -7,3 +7,10 @@ console.log(2*5);
 
 // Generar ventana
 console.log(window); // pertenece a front end
+
+console.log(global);
+
+//Imprimir en consola hola nodejs 1000 veces
+setInterval(function(){
+    console.log('hola nodejs')
+},1000)
