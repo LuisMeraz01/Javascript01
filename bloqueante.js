e87f1dfd57dc81fa23e7984a52153177c2054d6a
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-10-04 08:17:30 -0700

    Modelo bloqueante en nodejs

---
+++ bloqueante.js
@@ -0,0 +1,9 @@
+
+var f = require("fs");
+console.log("\nAbriendo el archivo");
+
+var content = fs.readFileSync("el_quijote.txt");
+console.log(content);
+
+console.log("\nHaciendo otra cosa");
+console.log(process.uptime());
