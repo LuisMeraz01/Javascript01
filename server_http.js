b60fb1277e1e0520a8dad349dc02d1ec106ea12d
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-10-05 08:55:04 -0700

    servidor completo funcionando

---
+++ server_http.js
@@ -0,0 +1,20 @@
+
+// Llamamos el modulo http
+const http = require('http');
+const hostname = '192.168.1.105';
+const port = 3000; // Buena practica del 3000 hacia arriba para no interferir 
+
+const server = http.createServer((req,res)=> {
+
+    res.statusCode = 200; // Codigo http 200 si todo sale bien -> mandara cabecera
+    res.setHeader ('Content-Type', 'text/plain');
+    res.end('Hola mundo\n');
+
+});
+
+server.listen(port, hostname, ()=>{
+
+    console.log('El servidor esta corriendo en http://${hostname}:${port}/');
+
+});
+
